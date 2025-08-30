f1460c5d9779c23240a24ab5dfb270442b4578c0
FernRiosA <friosayala@ucsb.edu>
2025-08-30 05:10:13 -0700

    Update constants.hpp

--- Lab4/constants.hpp
+++ Lab4/constants.hpp
@@ -1,3 +1,4 @@
+//modified for a file with 25 numbers
 #include <string>
 
 // BE VERY CAREFUL MODIFYING THIS FILE! 
@@ -9,6 +10,6 @@ const int SEARCHES[NSEARCHES] = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4};
 
 
 //Constants for the data file that your program is reading in
-const int MAXSIZE = 20;     // amount of integers in the file (you need this to declare your array size)
+const int MAXSIZE = 25;     // amount of integers in the file (you need this to declare your array size)
 const std::string FILENAME = "ArrayFile.txt";    // The file name with the integers
 
